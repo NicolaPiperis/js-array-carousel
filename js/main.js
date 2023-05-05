6fa8f624c1fe204185a440d9fc428b6869749305
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-05 17:21:56 +0200

    update

--- js/main.js
+++ js/main.js
@@ -10,17 +10,22 @@ console.log(img);
 // SELEZIONE CONTENITORE IMMAGINI
 let element = document.querySelector(".container_img");
 console.log(element);
+let image;
+
+// INDICE DI POSIZIONE
+let activeImage = 0;
+console.log(activeImage);
 
 // CICLIZZIAMO LE IMMAGINI
 for(let i = 0; i < img.length; i++) {   
     
-    let image = img [i]
+    let image = img[i];
     console.log(image);
-    element.innerHTML += `<img src="${image}">`;
-
-    // INDICE DI POSIZIONE
-    let imagePosition = i + 0;
-    console.log(imagePosition);
+    if(i === activeImage) {
+        element.innerHTML += `<img class="active" src="${image}">`;
+    } else {
+        element.innerHTML += `<img src="${image}">`;
+    }   
 }
 
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
@@ -31,19 +36,61 @@ let next = document.getElementById("next");
 let prev = document.getElementById("prev");
 console.log(next, prev);
 
+//Qua dovresti definirti un array con il riferimento a tutte le slide del dom ordinate
+let count = 0;
+
 next.addEventListener("click",
     function(){
 
-        if(imagePosition = image) {
-            element.classList.add("active");
-        }
+        // let imageCount = img[count];
+        // console.log(imageCount);
+        // count++;
+
+
+        // rimuovo dal div dell'img in cui suono posizionata la classe active
+        img[count].classList.remove("active");
+        
+        
+        // incremento il contatore dell'immagine su cui sono posizionata in questo momento
+        count++;
+        console.log(count);
+        
+        // and aggiungo la classe active al successivo
+        img[count].classList.add("active");
+        
     }
-)
-
-// Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-
-// Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. 
+    )
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+    
+    
+    // Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. 
+    
+    
+    
+    
+    // activeImage++;
+    
+
+    // if(activeImage = image) {
+    //     element.classList.add("active");
+    // }
