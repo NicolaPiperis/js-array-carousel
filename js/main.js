8145fe156f866f49da81d4d06aab31f6d31ac409
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-05 15:24:01 +0200

    function and if in progress

--- js/main.js
+++ js/main.js
@@ -3,31 +3,42 @@
 // Per prima cosa, creiamo il markup statico : costruiamo il container e inseriamo un’immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull’aspetto logico.
 
 // Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal.
+
+// ARRAY
 let img = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
 console.log(img);
+// SELEZIONE CONTENITORE IMMAGINI
 let element = document.querySelector(".container_img");
 console.log(element);
 
-
+// CICLIZZIAMO LE IMMAGINI
 for(let i = 0; i < img.length; i++) {   
-    imagePosition = 0 + i;
     
     let image = img [i]
+    console.log(image);
     element.innerHTML += `<img src="${image}">`;
 
-        if (imagePosition = i) {
-            element.classList.add("active");
-        }
+    // INDICE DI POSIZIONE
+    let imagePosition = i + 0;
+    console.log(imagePosition);
 }
-
-
-
-
-
-
 
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 
+
+// arrows dichiarazione
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+console.log(next, prev);
+
+next.addEventListener("click",
+    function(){
+
+        if(imagePosition = image) {
+            element.classList.add("active");
+        }
+    }
+)
 
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
 
